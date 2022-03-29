f8f3f96d11c2a678a3b3696193a3006eabc6f3a5
PrettyGaijin <marian.frische.96@googlemail.com>
2022-03-30 07:05:46 +0900

    Added Shareholder NFT Function + Visibility Fixes

--- KCCSocialTokenDistributor.sol
+++ KCCSocialTokenDistributor.sol
@@ -5,6 +5,7 @@ pragma solidity ^0.8.7;
 import "@openzeppelin/contracts/token/ERC20/IERC20.sol";
 import "@openzeppelin/contracts/utils/Address.sol";
 import "@openzeppelin/contracts/access/Ownable.sol";
+import "@openzeppelin/contracts/token/ERC1155/IERC1155.sol";
 
 
 contract KCCSocialTokenDistributor is Ownable {
@@ -23,9 +24,17 @@ contract KCCSocialTokenDistributor is Ownable {
     mapping(address => mapping(address => uint256)) _claimedTokens;
     mapping(address => mapping(address => uint256)) _unclaimedTokens;
 
+    uint256[] _nftShareIDs;
+    uint256 _totalNFTOwnerShare;
+    mapping(uint256 => uint256) _nftOwnerShares;
+    mapping(address => uint256) _nftOwnerTotalUnclaimedTokens;
+    mapping(uint256 => mapping(address => uint256)) _nftOwnerClaimedTokens;
+    mapping(uint256 => mapping(address => uint256)) _nftOwnerUnclaimedTokens;
+
     address[] _supportedTokens;
     mapping(address => bool) _isTokenSupported;
-    mapping(address => bool) _permittedStores;
+
+    IERC1155 public _KCCSocialNFT;
 
     constructor() {
         _supportedTokens.push(address(0));
@@ -33,12 +42,12 @@ contract KCCSocialTokenDistributor is Ownable {
 
     // Contract Control
 
-    function ChangePermittedStore(address token, bool allowed) external onlyOwner
+    function SetKCCSocialNFT(address nft) external onlyOwner
     {
-        _permittedStores[token] = allowed;
+        _KCCSocialNFT = IERC1155(nft);
     }
 
-    function AdjustUserGlobalShare(address user, uint256 share) internal
+    function AdjustUserGlobalShare(address user, uint256 share) external onlyOwner
     {
         UpdateTotalUnclaimedTokens();
 
@@ -72,25 +81,44 @@ contract KCCSocialTokenDistributor is Ownable {
         }
     }
 
+    function AddNFTShareHolderIDs(uint256[] calldata nftIDs, uint256 share) external onlyOwner
+    {
+        UpdateNFTOwnerUnclaimedTokens();
+
+        _totalNFTOwnerShare += share * nftIDs.length;
+        for(uint256 i = 0; i < nftIDs.length; i++) {
+            _nftShareIDs.push(nftIDs[i]);
+            _nftOwnerShares[nftIDs[i]] = share;
+        }
+    }
+
     // Add Payment (In)
 
     function AddExternalTokenPayment(uint256 accessoryID, address token, uint256 amount) public
     {
-        require(_permittedStores[_msgSender()], "You are not allowed to do this.");
+        require(address(_KCCSocialNFT) == _msgSender(), "You are not allowed to do this.");
 
         if(!_isTokenSupported[token]) {
             _supportedTokens.push(token);
             _isTokenSupported[token] = true;
         }
 
+        uint256 remainingTokens = amount;
+
+        if(_totalNFTOwnerShare > 0) {
+            uint256 nftOwnerShare = CalculateShare(remainingTokens, _totalNFTOwnerShare);
+            _nftOwnerTotalUnclaimedTokens[token] += nftOwnerShare;
+            remainingTokens -= nftOwnerShare;
+        }
+
         PartnerShare storage parnterShare = _partnerShares[accessoryID];
         if(parnterShare.exists) {
-            uint256 partnerShare = CalculateShare(amount, parnterShare.share);
+            uint256 partnerShare = CalculateShare(remainingTokens, parnterShare.share);
             _partnerUnclaimedTokens[parnterShare.wallet][token] += partnerShare;
-            _internalUnclaimedTokens[token] += amount - partnerShare;
+            _internalUnclaimedTokens[token] += remainingTokens - partnerShare;
         }
         else {
-            _internalUnclaimedTokens[token] += amount;
+            _internalUnclaimedTokens[token] += remainingTokens;
         }
     }
 
@@ -131,6 +159,15 @@ contract KCCSocialTokenDistributor is Ownable {
         return unclaimedTokens;
     }
 
+    function TotalNFTUnclaimedIncome(uint256 nft, address token) internal view returns (uint256)
+    {
+        uint256 unclaimedNFTIncome = (_nftOwnerTotalUnclaimedTokens[token] * _nftOwnerShares[nft]) / _totalNFTOwnerShare;
+
+        unclaimedNFTIncome -= _nftOwnerClaimedTokens[nft][token];
+        unclaimedNFTIncome += _nftOwnerUnclaimedTokens[nft][token];
+        return unclaimedNFTIncome;
+    }
+
     function UpdateTotalUnclaimedTokens() internal
     {
         for(uint256 i = 0; i < _supportedTokens.length; i++) {
@@ -139,18 +176,62 @@ contract KCCSocialTokenDistributor is Ownable {
             for(uint256 j = 0; j < _globalShareOwners.length; j++) {
                 address user = _globalShareOwners[j];
                 _unclaimedTokens[user][token] = TotalGlobalUnclaimedIncome(user, token);
+                _claimedTokens[user][token] = 0;
             }
 
             _internalUnclaimedTokens[token] = 0;
         }
     }
 
+    function UpdateNFTOwnerUnclaimedTokens() public onlyOwner
+    {
+        for(uint256 i = 0; i < _supportedTokens.length; i++) {
+            address token = _supportedTokens[i];
+
+            for(uint256 j = 0; j < _nftShareIDs.length; j++) {
+                uint256 nftID = _nftShareIDs[j];
+                _nftOwnerUnclaimedTokens[nftID][token] = TotalNFTUnclaimedIncome(nftID, token);
+                _nftOwnerClaimedTokens[nftID][token] = 0;
+            }
+
+            _nftOwnerTotalUnclaimedTokens[token] = 0;
+        }
+    }
+
+    function TotalUnclaimedIncome(address token, address user, uint256[] calldata ownedNFTs) public view returns (uint256)
+    {
+        uint256 globalClaimed = TotalGlobalUnclaimedIncome(user, token);
+        uint256 parnerClaim = _partnerUnclaimedTokens[user][token];
+        uint256 nftClaim = 0;
+
+        for(uint256 i = 0; i < ownedNFTs.length; i++) {
+            require(_KCCSocialNFT.balanceOf(user, ownedNFTs[i]) > 0, "You do not own these share holder NFTs");
+
+            nftClaim += TotalNFTUnclaimedIncome(ownedNFTs[i], token);
+        }
+
+        uint256 totalUnclaimed = globalClaimed + nftClaim + parnerClaim;
+
+        return totalUnclaimed;
+    }
+
     // Withdraw
 
-    function WithdrawToken(address token, address user) internal
+    function WithdrawToken(address token, address user, uint256[] calldata ownedNFTs) internal
     {
         uint256 unclaimed = TotalGlobalUnclaimedIncome(user, token);
         uint256 totalWithdraw = unclaimed + _partnerUnclaimedTokens[user][token];
+        uint256 nftClaim = 0;
+
+        for(uint256 i = 0; i < ownedNFTs.length; i++) {
+            require(_KCCSocialNFT.balanceOf(user, ownedNFTs[i]) > 0, "You do not own these share holder NFTs");
+
+            nftClaim += TotalNFTUnclaimedIncome(ownedNFTs[i], token);
+            _nftOwnerUnclaimedTokens[ownedNFTs[i]][token] = 0;
+            _nftOwnerClaimedTokens[ownedNFTs[i]][token] += nftClaim;
+        }
+
+        totalWithdraw += nftClaim;
 
         _unclaimedTokens[user][token] = 0;
         _claimedTokens[user][token] += unclaimed;
@@ -166,10 +247,10 @@ contract KCCSocialTokenDistributor is Ownable {
         }
     }
 
-    function WithdrawShares() external
+    function WithdrawShares(uint256[] calldata ownedNFTs) external
     {
         for(uint256 i = 0; i < _supportedTokens.length; i++) {
-            WithdrawToken(_supportedTokens[i], _msgSender());
+            WithdrawToken(_supportedTokens[i], _msgSender(), ownedNFTs);
         }
     }
 }
