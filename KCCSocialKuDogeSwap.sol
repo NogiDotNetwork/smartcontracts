fa11dfd30ca7257cd3d818f7b027b2ff06e5982a
PrettyGaijin <marian.frische.96@googlemail.com>
2022-04-02 01:40:26 +0900

    Update KCCSocialKuDogeSwap.sol

--- KCCSocialKuDogeSwap.sol
+++ KCCSocialKuDogeSwap.sol
@@ -5,21 +5,20 @@ pragma solidity ^0.8.7;
 import "@openzeppelin/contracts/utils/Address.sol";
 import "@openzeppelin/contracts/access/Ownable.sol";
 import "@openzeppelin/contracts/token/ERC1155/IERC1155.sol";
+import "@openzeppelin/contracts/token/ERC20/IERC20.sol";
 
 interface IKuswapRouter02 {
     function WETH() external pure returns (address);
 
-    function swapExactETHForTokens(uint amountOutMin, address[] calldata path, address to, uint deadline)
-        external
-        payable
-        returns (uint[] memory amounts);
+    function swapExactETHForTokens(uint amountOutMin, address[] calldata path, address to, uint deadline) external payable returns (uint[] memory amounts);
 
     function getAmountsOut(uint amountIn, address[] calldata path) external view returns (uint[] memory amounts);
 }
 
 contract KCCSocialKuDogeSwap is Ownable {
     IKuswapRouter02 public _router = IKuswapRouter02(0xc5f442007e08e3b13C9f95fA22F2a2B9369d7C8C);
-    IERC1155 public _KCCSocialNFT;
+    IERC20 public _KuDoge = IERC20(0xe9B9106731D200fEc5335E1E01A26ec92624724B);
+    IERC1155 public _KCCSocialNFT = IERC1155(0x618D116454A1AEdB0c6Cf2C714FB19c4c46bE0Bb);
 
     mapping(uint256 => bool) _isNFTIDPermittedToUseSwap;
 
@@ -31,6 +30,11 @@ contract KCCSocialKuDogeSwap is Ownable {
     function SetRouter(address router) external onlyOwner
     {
         _router = IKuswapRouter02(router);
+    }
+
+    function SetKuDoge(address kudo) external onlyOwner
+    {
+        _KuDoge = IERC20(kudo);
     }
 
     function SetKCCSocialNFT(address nft) external onlyOwner
@@ -49,31 +53,43 @@ contract KCCSocialKuDogeSwap is Ownable {
 
     function swapExactETHForTokens(uint amountOutMin, uint256 usingNFTID) external payable returns (uint[] memory amounts)
     {
+        //Require ownership of correct KCCSocial NFTs that enable the feeless KuDoge Swap
         require(_isNFTIDPermittedToUseSwap[usingNFTID], "This NFT does not unlock the feeless swap");
         require(_KCCSocialNFT.balanceOf(msg.sender, usingNFTID) > 0, "You do not own this NFT");
 
-        uint256 ethBalanceBeforeSwap = address(this).balance - msg.value;
-
         address[] memory path = new address[](2);
         path[0] = _router.WETH();
-        path[1] = 0xe9B9106731D200fEc5335E1E01A26ec92624724B;
+        path[1] = address(_KuDoge);
 
-        amounts = _router.swapExactETHForTokens{value:msg.value}(amountOutMin, path, msg.sender, block.timestamp);
+        //This contract is feeless, so swapped tokens should be received by this and then forwarded
+        uint256 preSwapKuDogeBalance = _KuDoge.balanceOf(address(this));
 
-        uint256 differenceETHBalance = address(this).balance - ethBalanceBeforeSwap;
+        amounts = _router.swapExactETHForTokens{value:msg.value}(amountOutMin, path, address(this), block.timestamp);
 
-        //Refund any dust that was left over
-        if(differenceETHBalance > 0) {
-            Address.sendValue(payable(msg.sender), differenceETHBalance);
-        }
+        uint256 afterSwapKuDogeBalance = _KuDoge.balanceOf(address(this));
+        uint256 swappedKuDoge = afterSwapKuDogeBalance - preSwapKuDogeBalance;
+
+        _KuDoge.transfer(msg.sender, swappedKuDoge);
     }
 
     function getAmountsOut(uint amountIn) external view returns (uint[] memory amounts)
     {
         address[] memory path = new address[](2);
         path[0] = _router.WETH();
-        path[1] = 0xe9B9106731D200fEc5335E1E01A26ec92624724B;
+        path[1] = address(_KuDoge);
 
         amounts = _router.getAmountsOut(amountIn, path);
+    }
+
+    // Safety Fallbacks in case someone sends crypto to this contract
+
+    function WithdrawKCS() external onlyOwner
+    {
+        Address.sendValue(payable(owner()), address(this).balance);
+    }
+
+    function WithdrawAnyToken(address token) external onlyOwner
+    {
+        IERC20(token).transfer(owner(), IERC20(token).balanceOf(address(this)));
     }
 }
